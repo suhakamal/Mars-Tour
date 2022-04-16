4a1565c3d40a01a248a8e48818c5978873c73b64
Suha Kamal <suhakamal6@gmail.com>
2022-04-16 11:10:04 +0200

    Add files via upload

---
+++ process.php
@@ -0,0 +1,11 @@
+<?php
+require'connect.php';
+$first = $last  = $email = $mobile = $ticket = ''; 
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+        $sql = "INSERT INTO registration (First, Last, Email, Mobile, Ticket) VALUES ('$_POST[first]', '$_POST[last]', '$_POST[email]', '$_POST[mobile]', '$_POST[ticket]')";
+        if (mysqli_query($conn,$sql)) {
+            header("Location: index.html");
+        } else {
+            echo "Registered Failed";
+        }       
+}
