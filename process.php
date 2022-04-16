b5bbee63449c292fcfeb5057b537667d04d8b060
Suha Kamal <suhakamal6@gmail.com>
2022-04-16 12:53:21 +0200

    Update process.php

--- process.php
+++ process.php
@@ -4,7 +4,8 @@ $first = $last  = $email = $mobile = $ticket = '';
     if($_SERVER["REQUEST_METHOD"]=="POST"){
         $sql = "INSERT INTO registration (First, Last, Email, Mobile, Ticket) VALUES ('$_POST[first]', '$_POST[last]', '$_POST[email]', '$_POST[mobile]', '$_POST[ticket]')";
         if (mysqli_query($conn,$sql)) {
-            header("Location: index.html");
+            sleep(15);
+            header("Location: index.php");
         } else {
             echo "Registered Failed";
         }       
